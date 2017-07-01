40bb0525a031901cd626391454ffcbb496c7d83d
bipindr123 <bipindr123@gmail.com>
2017-07-01 20:57:37 +0530

    added validation

--- index.php
+++ index.php
@@ -7,14 +7,15 @@
 <body>
 <h1>ENTER DATA</h1>
 <br>
-<form action="welcome.php" method="post">
-
-    Name: <input type="text" name="name"><br>
-    Email: <input type="email" name="email"><br>
-    Phone: <input type="number" name="phone"><br>
-    <input type="submit">
+<form method="post" action="welcome.php ">
+    Name: <input type="text" name="name"  pattern="^[a-zA-Z ]*$" required>
+    <br><br>
+    Email: <input type="email" name="email" required>
+    <br><br>
+    Phone: <input type="text" name="phone" pattern="^[0-9]{10}$" required>
+    <br><br>
+    <input type="submit" name="submit">
 </form>
-
 
 </body>
 
