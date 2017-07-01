227bc1c77e434ae44187e5210cd6a8934239ab8f
bipindr123 <bipindr123@gmail.com>
2017-07-01 18:59:21 +0530

    somechanges

---
+++ fire.js
@@ -0,0 +1,13 @@
+/**
+ * Created by bipin on 01-07-2017.
+ */
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyDi1w-MHCTSWtxDZU8AloQexBCWpHNxOcY",
+    authDomain: "testphp-e136c.firebaseapp.com",
+    databaseURL: "https://testphp-e136c.firebaseio.com",
+    projectId: "testphp-e136c",
+    storageBucket: "testphp-e136c.appspot.com",
+    messagingSenderId: "603290260630"
+};
+firebase.initializeApp(config);
